88b9fe91255d4d4dd682bd4fd3a35eb63e607038
Sia <siakaramalegos@gmail.com>
2015-04-07 15:15:48 -0500

    Simplify drawing of canvas back to original set up

--- js/engine.js
+++ js/engine.js
@@ -21,8 +21,7 @@ var Engine = (function(global) {
      */
     var doc = global.document,
         win = global.window,
-        // canvas = doc.createElement('canvas'),
-        canvas = document.querySelector('canvas'),
+        canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
         lastTime;
 
